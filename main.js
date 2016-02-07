64d6749368b58ba15a0eed673f6c00d15eec5088
Ethan Goldberg <ethanjgoldberg@gmail.com>
2016-02-06 22:15:33 -0800

    got them arrays. more trouble than it looked.

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var AP = function (context, defaultValue) {
     audioParam = gainNode.gain;
     audioParam.value = defaultValue;
     audioParam.connect = function (destination, input) {
-	gainNode.connect(destination);
+	gainNode.connect(destination, 0, input);
     }
     audioParam._nodes = [scriptNode, gainNode];
     return audioParam;
@@ -28,7 +28,8 @@ var Attr = React.createClass({
 		<div className="attr">
 		{this.props.name}:
 	    { React.cloneElement(React.Children.only(this.props.children), {
-		to: this.props.to
+		to: this.props.to,
+		name: this.props.name
 	    })
 	    }
 	    </div>
@@ -43,7 +44,7 @@ var Value = React.createClass({
 	};
     },
     connect: function (to) {
-	console.log(this.props.to);
+	console.log(this.props.to, this.props);
 	to.value = this.state.value;
     },
     handleChange: function (event) {
@@ -54,6 +55,26 @@ var Value = React.createClass({
     render: function () {
 	this.connect(this.props.to);
 	return (<input type="number" value={this.state.value} onChange={this.handleChange} />);
+    }
+});
+
+var List = React.createClass({
+    getInitialState: function () {
+	return {
+	    value: this.props.val
+	};
+    },
+    connect: function (to) {
+	to.value = this.state.value;
+    },
+    handleChange: function (event) {
+	this.setState({
+	    value: event.target.value.split(/[ ,]+/)
+	});
+    },
+    render: function () {
+	this.connect(this.props.to);
+	return (<input type="text" value={this.state.value} onChange={this.handleChange} />);
     }
 });
 
@@ -104,31 +125,75 @@ var Osc = React.createClass({
     }
 });
 
+var Array = React.createClass({
+    getInitialState: function () {
+	var BS = 4096 * 4;
+	var node = ctx.createScriptProcessor(BS, 1, 1);
+	node.onaudioprocess = function (event) {
+	    var idx = this.state.idx || 0;
+	    var lastChange = this.state.lastChange || 0;
+	    var frequency = event.inputBuffer.getChannelData(0);
+	    //var base = event.inputBuffer.getChannelData(1);
+	    var output = event.outputBuffer.getChannelData(0)
+
+	    for (var i = 0; i < BS; i++) {
+		var samplesPerCycle = ctx.sampleRate / frequency[i];
+		if (i >= lastChange + samplesPerCycle) {
+		    lastChange = i;
+		    idx++;
+		    while (idx >= this.state.vals.value.length) {
+			idx -= this.state.vals.value.length;
+		    }
+		}
+		output[i] = this.state.vals.value[idx]// * base[i];
+	    }
+	    this.state.idx = idx;
+	    this.state.lastChange = lastChange;
+	    
+	    this.state.lastChange -= BS;
+	}.bind(this);
+	
+	var gain = ctx.createGain();
+	node.connect(gain);
+	
+	var frequency = AP(ctx, 0);
+	frequency.connect(node);
+	
+	return {
+	    vals: { value: [] },
+	    node: node,
+	    gain: gain,
+	    frequency: frequency
+	}
+    },
+    connect: function (to) {
+	this.state.gain.connect(to);
+    },
+    render: function () {
+	this.connect(this.props.to);
+
+	return (<div className="node">
+		{ React.Children.map(this.props.children, function (c) {
+		    var to = null;
+		    if (c.props.name == 'vals') {
+			to = this.state.vals;
+		    } else if (c.props.name == 'base') {
+			to = this.state.gain.gain;
+		    } else if (c.props.name == 'frequency') {
+			to = this.state.frequency;
+		    }
+		    if (to == null) return c;
+		    return React.cloneElement(c, {
+			to: to
+		    });
+		}.bind(this))
+		}
+		</div>);
+    }
+});
+
 var Dest = React.createClass({
     render: function () {
-
-	/*
-	var osc1 = ctx.createOscillator();
-	var osc2 = ctx.createOscillator();
-	var gain = ctx.createGain();
-	var proc = AP(ctx, 1000);
-	
-	osc2.frequency.value = 0;
-	
-	osc1.frequency.value = .25;
-	osc1.connect(gain);
-	osc1.start();
-	gain.gain.value = 1000;
-
-	gain.connect(proc);
-
-	proc.connect(osc2.frequency);
-	
-	osc2.connect(ctx.destination);
-	osc2.start();
-	return (<div></div>);
-*/
-	
 	return (<div>
 		{ React.Children.map(this.props.children, function (c) {
 		    return React.cloneElement(c, {
@@ -137,19 +202,40 @@ var Dest = React.createClass({
 		}) }
 		</div>
 	       );
-	/*
-			ref: function (ch) {
-			    ch.connect(ctx.destination);
-			}.bind(this)
-		    });
-		}, this)
-		}
-		</div>
-	       );
-*/
     }
 });
 
+ReactDOM.render(
+	<Dest>
+	<Osc name="fm'ed">
+	
+	<Attr name='frequency'>
+	<Array name='mel'>
+	<Attr name='vals'><List val={[1, 2, 1.8, 1.5]} /></Attr>
+	<Attr name='base'>
+	<Array name='blue'>
+	<Attr name='vals'><List val={[1,1,1,1,1.33,1.33,1,1, 1.5, 1.33, 1, 1]} /></Attr>
+	<Attr name='base'><Value val={110} /></Attr>
+	<Attr name='frequency'><Value val={.25} /></Attr>
+	</Array>
+	</Attr>
+	<Attr name='frequency'><Value val={1} /></Attr>
+	</Array>
+	</Attr>
+
+	<Attr name='gain'>
+	<Value val={1}></Value>
+	</Attr>
+
+	<Attr name='offset'>
+	<Value val={0}></Value>
+	</Attr>
+	</Osc>
+	</Dest>,
+    document.getElementById('root')
+);
+
+/*
 ReactDOM.render(
 	<Dest>
 	<Osc name="fm'ed">
@@ -163,11 +249,21 @@ ReactDOM.render(
 	</Attr>
 	
 	<Attr name='gain'>
-	<Value val={220}></Value>
+	<Value val={20}></Value>
 	</Attr>
 
 	<Attr name='offset'>
-	<Value val={220}></Value>
+	<Array name='mel'>
+	<Attr name='vals'><List val={[1, 2, 1.8, 1.5]} /></Attr>
+	<Attr name='base'>
+	<Array name='blue'>
+	<Attr name='vals'><List val={[1, 1, 1, 1, 1.33, 1.33, 1, 1, 1.5, 1.33, 1, 1]} /></Attr>
+	<Attr name='base'><Value val={110} /></Attr>
+	<Attr name='duration'><Value val={4} /></Attr>
+	</Array>
+	</Attr>
+	<Attr name='duration'><Value val={1} /></Attr>
+	</Array>
 	</Attr>
 	
 	</Osc>
@@ -186,3 +282,4 @@ ReactDOM.render(
 	</Dest>,
     document.getElementById('root')
 );
+*/
